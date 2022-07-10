9ce8440da911bfebb3df8ca8b2976bffb202c2ab
장병희 <jangbyeonghui@jangbyeonghuiui-MacBookPro.local>
2022-07-10 17:51:25 +0900

    CB-10: [feat]: attach test code

--- src/main/java/com/pinkdumbell/cocobob/CocobobApplication.java
+++ src/main/java/com/pinkdumbell/cocobob/CocobobApplication.java
@@ -2,10 +2,16 @@ package com.pinkdumbell.cocobob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CocobobApplication {
-
+	@RequestMapping("/")
+	public String home() {
+		return "Hello CoCoBob Backend";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CocobobApplication.class, args);
 	}
