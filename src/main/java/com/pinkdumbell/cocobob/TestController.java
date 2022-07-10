e812441fa0dc5a4fcc25d0801a0772de0763c187
ErranderLee <dre12am14@gmail.com>
2022-07-10 11:09:36 +0900

    # CB-9 test: API 서버 환경 구축 # write testController
    ################
    - 무중단 배포를 위한 테스트 컨트롤러 작성
    
    ################

---
+++ src/main/java/com/pinkdumbell/cocobob/TestController.java
@@ -0,0 +1,16 @@
+package com.pinkdumbell.cocobob;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @Value("${server.port}")
+    private String portNum;
+
+    @GetMapping("")
+    public String test() {
+        return portNum;
+    }
+}
