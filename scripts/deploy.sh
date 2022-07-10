3d2e4788c2e0bfd90b4bef6ef4bbd4cd715dffde
장병희 <jangbyeonghui@jangbyeonghuiui-MacBookPro.local>
2022-07-10 11:46:24 +0900

    CB-10 [feat]: attach script file
    스크립트 파일 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/CocoBob-BE/travis
+REPOSITORY=/home/ec2-user/CocoBob-BE/travis/build
 PROJECT_NAME=cocobob-be-build
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/build/*.jar $REPOSITORY/
+cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
@@ -33,7 +33,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-    -Dspring.profiles.active=real \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar /home/ec2-user/CocoBob-BE/travis/build/build/libs/cocobob-0.0.1-SNAPSHOT.jar
