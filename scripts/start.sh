77f93494a368a991a8e845b5c9848dd87e0b2d28
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-07-12 02:54:14 +0900

    add restart docker scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -29,5 +29,8 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 cd $REPOSITORY
 
+sudo systemctl start docker
+sudo systemctl enable docker
+
 docker build -t spring ./
 docker run -it --name "$IDLE_PROFILE" -d -e active=$IDLE_PROFILE -p $IDLE_PORT:$IDLE_PORT spring
