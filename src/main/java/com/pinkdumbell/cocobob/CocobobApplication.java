4dfa0eb3a45e55ce2b5e318fa3b06915aea70cde
ErranderLee <dre12am14@gmail.com>
2022-07-12 01:23:17 +0900

    # CB-9 test: API 서버 환경 구축 # add code deploy
    ################
    - code deploy 테스트
    
    ################

--- src/main/java/com/pinkdumbell/cocobob/CocobobApplication.java
+++ src/main/java/com/pinkdumbell/cocobob/CocobobApplication.java
@@ -9,6 +9,6 @@ public class CocobobApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CocobobApplication.class, args);
 		System.out.println("test");
-
+		System.out.println("test");
 	}
 }
