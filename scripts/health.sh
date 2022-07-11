2ebd16270739fbcbf50c65e5be7529e52064722e
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-07-12 03:04:52 +0900

    CB-10 [refactor]: typo EC2 Address

--- scripts/health.sh
+++ scripts/health.sh
@@ -9,12 +9,12 @@ IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
-echo "> curl -s http://54.180.115.150/:$IDLE_PORT/"
+echo "> curl -s http://54.180.115.150:$IDLE_PORT/"
 sleep 10
 
 for RETRY_COUNT in {1..10}
 do
-  RESPONSE=$(curl -s http://54.180.115.150/:${IDLE_PORT})
+  RESPONSE=$(curl -s http://54.180.115.150:${IDLE_PORT})
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
