c64e28af8717a45ebc26199e11c247c0932fcddc
ErranderLee <dre12am14@gmail.com>
2022-07-12 14:42:23 +0900

    # CB-9 test: API 서버 환경 구축 # add scripts
    ################
    - 배포 스크립트 추가
    
    ################

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,10 +14,11 @@ docker rmi -f $(IMAGE_URI)
 
 echo "> Pull Image From ECR"
 echo "> docker pull $(IMAGE_URI)"
+docker pull $(IMAGE_URI)
 
 echo "> 새 어플리케이션 배포"
 
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> 새로운 어플리케이션을 profile=$IDLE_PROFILE 로 실행합니다."
-docker run -it --name "$IDLE_PROFILE" -d -e active=$IDLE_PROFILE -p $IDLE_PORT:$IDLE_PORT spring
+docker run -it --name "$IDLE_PROFILE" -d -e active=$IDLE_PROFILE -p $IDLE_PORT:$IDLE_PORT
