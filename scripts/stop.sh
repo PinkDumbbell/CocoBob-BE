6e4cd14e79738b6de64dfdf22d5357e38440890e
ErranderLee <dre12am14@gmail.com>
2022-07-12 17:08:09 +0900

    # CB-9 test: API 서버 환경 구축 # add scripts
    ################
    - 배포 스크립트 추가
    
    ################

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -15,10 +15,12 @@ if [ -z ${CONTAINER_ID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-#  echo "> docker stop ${IDLE_PROFILE}" > ~/deploy-log.log
-  echo "sudo docker stop ${IDLE_PROFILE} && sudo docker rm ${IDLE_PROFILE}"
-  sudo docker stop -f ${IDLE_PROFILE} && sudo docker rm -f ${IDLE_PROFILE} > ~/deploy-log.log
-#  echo "> docker rm ${IDLE_PROFILE}" > ~/deploy-log.log
-#  sudo docker rm ${IDLE_PROFILE} > ~/deploy-log.log
+  sudo rm -f ~/deploy-log.log
+  sudo touch ~/deploy-log.log
+
+  echo "> docker stop" ${IDLE_PROFILE} > ~/deploy-log.log
+  sudo docker stop -f ${IDLE_PROFILE} > ~/deploy-log.log
+  echo "> docker rm" ${IDLE_PROFILE} > ~/deploy-log.log
+  sudo docker rm ${IDLE_PROFILE} > ~/deploy-log.log
   sleep 5
 fi
