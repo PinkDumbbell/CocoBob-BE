22cb4f114571fb590a44e79ae7bc8055547cb92b
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-07-12 17:28:39 +0900

    CB-10 [refactor] write stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -18,5 +18,5 @@ else
   echo "> docker stop ${IDLE_PROFILE}"
   echo "> docker rm ${IDLE_PROFILE}"
   sudo docker stop ${IDLE_PROFILE} && sudo docker container rm -f ${IDLE_PROFILE}    # 컨테이너 이름을 지정해서 사용하기 때문에.. 꼭 컨테이너 삭제도 같이 해주셔야 합니다. (나중에 다시 띄울거기 때문에..)
-  sleep 15
+  sleep 80
 fi
