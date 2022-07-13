aa5c5edb04742b71d7acccc5ba6216637d26c2a1
ErranderLee <dre12am14@gmail.com>
2022-07-13 16:25:53 +0900

    # CB-117 test: test swagger # test swagger whether working or not
    ################
    - ProfileController에 swagger 관련 기능을 붙여 올바르게 swagger가 동작하는지 테스트
    
    ################

--- src/main/java/com/pinkdumbell/cocobob/ProfileController.java
+++ src/main/java/com/pinkdumbell/cocobob/ProfileController.java
@@ -1,7 +1,7 @@
 package com.pinkdumbell.cocobob;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +11,20 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class PortController {
+@ApiOperation(
+        value = "Profile API",
+        notes = "무중단 배포를 위한 프로파일에 관한 컨트롤러"
+)
+public class ProfileController {
 
     private final Environment env;
-    
-    @Value("${server.port}")
-    private String portNum;
 
+    @ApiOperation(
+            value = "Return Current Profile",
+            notes = "현재 사용되는 프로파일을 반환한다."
+    )
     @GetMapping("")
-    public String getCurrentPortNum () {
+    public String getCurrentProfile () {
         List<String> profile = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real1", "real2");
         String defaultProfile = profile.isEmpty() ? "default" : profile.get(0);
