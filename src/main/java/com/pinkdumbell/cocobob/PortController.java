96478a433d15e4d9ea326cb8c1033b4289522f68
ErranderLee <dre12am14@gmail.com>
2022-07-13 15:54:32 +0900

    # CB-9 refactor: API 서버 환경 구축 # 컨트롤러, 메서드 이름 수정
    ################
    - port 반환 메서드 이름, 컨트롤러 이름 수정
    
    ################

--- src/main/java/com/pinkdumbell/cocobob/PortController.java
+++ src/main/java/com/pinkdumbell/cocobob/PortController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class TestController {
+public class PortController {
 
     private final Environment env;
     
@@ -19,7 +19,7 @@ public class TestController {
     private String portNum;
 
     @GetMapping("")
-    public String test() {
+    public String getCurrentPortNum () {
         List<String> profile = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real1", "real2");
         String defaultProfile = profile.isEmpty() ? "default" : profile.get(0);
