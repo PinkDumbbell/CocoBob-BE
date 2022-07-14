2f71d4b1574d20090249b899fca74bc7ef2d7b90
ErranderLee <dre12am14@gmail.com>
2022-07-14 13:11:44 +0900

    # CB-91 feat: link uri # ################ - link uri to signup
    ################
    - 회원가입을 위한 uri 맵핑

--- src/main/java/com/pinkdumbell/cocobob/domain/user/UserController.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/user/UserController.java
@@ -1,6 +1,11 @@
 package com.pinkdumbell.cocobob.domain.user;
 
+import com.pinkdumbell.cocobob.domain.user.dto.UserCreateRequestDto;
+import com.pinkdumbell.cocobob.domain.user.dto.UserCreateResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/new")
+    public ResponseEntity<UserCreateResponseDto> signup(@RequestBody UserCreateRequestDto requestDto) {
+        return ResponseEntity.ok(userService.signup(requestDto));
+    }
 }
