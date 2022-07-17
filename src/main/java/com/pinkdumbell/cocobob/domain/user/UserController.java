bf38564a91cbbe72c8c348363a36f3af7a889b97
ErranderLee <dre12am14@gmail.com>
2022-07-17 15:42:39 +0900

    # CB-91 feat: add validation for UserCreateRequestDto # ################ - add validation to signup method in UserController for validation UserCreateRequestDto ################ - UserController에 DTO 검증을 위한 어노테이션을 추가.

--- src/main/java/com/pinkdumbell/cocobob/domain/user/UserController.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/user/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
 @RestController
@@ -16,7 +18,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/new")
-    public ResponseEntity<UserCreateResponseDto> signup(@RequestBody UserCreateRequestDto requestDto) {
+    public ResponseEntity<UserCreateResponseDto> signup(@RequestBody @Valid UserCreateRequestDto requestDto) {
         return ResponseEntity.ok(userService.signup(requestDto));
     }
 }
