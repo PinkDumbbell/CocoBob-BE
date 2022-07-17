3032f264ef4705e7a8a90f7b131825902cad66e6
ErranderLee <dre12am14@gmail.com>
2022-07-17 19:15:04 +0900

    # CB-91 test : add description to test # ################ - add description to test ################ - 회원가입 메서드 부가 설명 추가.

--- src/test/java/com/pinkdumbell/cocobob/domain/user/UserServiceTest.java
+++ src/test/java/com/pinkdumbell/cocobob/domain/user/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.pinkdumbell.cocobob.domain.user;
 import com.pinkdumbell.cocobob.domain.user.dto.UserCreateRequestDto;
 import com.pinkdumbell.cocobob.domain.user.dto.UserCreateResponseDto;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +19,8 @@ class UserServiceTest {
     UserService userService;
 
     @Test
-    void signup() {
+    @DisplayName("주어진 사용자 정보를 통해 회원가입을 할 수 있다.")
+    void testSignup() {
         UserCreateRequestDto requestDto = UserCreateRequestDto.builder()
                 .username("테스터")
                 .email("test@test.com")
