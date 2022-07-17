36de1cad399c0e5989a00bc0b6a235028f9feac2
ErranderLee <dre12am14@gmail.com>
2022-07-17 16:08:13 +0900

    # CB-91 fix: fix User signup # ################ - fix User signup in UserService ################ - findByEmail을 통해 기존에 입력한 이메일을 가진 사용자가 존재하는지 확인하고 있다면 예외를 던진다. 없으면 사용자 엔티티를 생성하여 저장한다.

--- src/main/java/com/pinkdumbell/cocobob/domain/user/UserService.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/user/UserService.java
@@ -5,13 +5,25 @@ import com.pinkdumbell.cocobob.domain.user.dto.UserCreateResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
     public UserCreateResponseDto signup(UserCreateRequestDto requestDto) {
-        return new UserCreateResponseDto(userRepository.save(userRepository.findByEmail(requestDto.getEmail())
-                .orElseThrow(() -> new RuntimeException("해당 이메일을 가진 사용자가 존재합니다."))));
+        Optional<User> user = userRepository.findByEmail(requestDto.getEmail());
+
+        if (user.isPresent()) {
+            throw new RuntimeException("해당 이메일을 가진 사용자가 이미 존재합니다.");
+        }
+
+        return new UserCreateResponseDto(
+                userRepository.save(User.builder()
+                                .email(requestDto.getEmail())
+                                .username(requestDto.getUsername())
+                                .password(requestDto.getPassword())
+                                .build()));
     }
 }
