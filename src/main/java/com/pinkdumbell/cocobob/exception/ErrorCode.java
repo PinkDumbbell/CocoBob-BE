e584698d08208a11c0ff4df32db916b10bd43a1c
ErranderLee <dre12am14@gmail.com>
2022-07-17 19:20:00 +0900

    # CB-91 chore : redefine http status code # ################ - redefine http status code when receive duplicate email ################ - 중복된 이메일로 가입을 시도했을 때 409 conflict 를 응답한다.

--- src/main/java/com/pinkdumbell/cocobob/exception/ErrorCode.java
+++ src/main/java/com/pinkdumbell/cocobob/exception/ErrorCode.java
@@ -3,15 +3,10 @@ package com.pinkdumbell.cocobob.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-
-
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    DUPLICATED_EMAIL(BAD_REQUEST, "해당 이메일을 가진 사용자가 존재합니다."),
+    DUPLICATED_EMAIL(HttpStatus.CONFLICT, "해당 이메일을 가진 사용자가 존재합니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL SERVER ERROR")
     ;
 
