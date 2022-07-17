55b72a6c6ecdb74187a5798b3a6295a72e2f9e61
ErranderLee <dre12am14@gmail.com>
2022-07-17 19:38:56 +0900

    # CB-91 chore : add swagger description # ################ - add swagger description with status code and examples ################ - 스웨거를 통하여 api 명세와 예시를 작성.

--- src/main/java/com/pinkdumbell/cocobob/domain/user/UserController.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/user/UserController.java
@@ -2,6 +2,9 @@ package com.pinkdumbell.cocobob.domain.user;
 
 import com.pinkdumbell.cocobob.domain.user.dto.UserCreateRequestDto;
 import com.pinkdumbell.cocobob.domain.user.dto.UserCreateResponseDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@ApiOperation("User API")
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
 @RestController
 public class UserController {
     private final UserService userService;
 
+    @ApiOperation(value = "signup", notes = "서비스 자체 회원가입")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "", response = UserCreateResponseDto.class),
+            @ApiResponse(code = 409, message = "해당 이메일을 가진 사용자가 존재합니다.")
+    })
     @PostMapping("/new")
     public ResponseEntity<UserCreateResponseDto> signup(@RequestBody @Valid UserCreateRequestDto requestDto) {
         return ResponseEntity.ok(userService.signup(requestDto));
--- src/main/java/com/pinkdumbell/cocobob/domain/user/dto/UserCreateRequestDto.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/user/dto/UserCreateRequestDto.java
@@ -1,5 +1,6 @@
 package com.pinkdumbell.cocobob.domain.user.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.constraints.Email;
@@ -12,11 +13,14 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @Getter
 public class UserCreateRequestDto {
+    @ApiModelProperty(notes = "사용자 이름", example = "이호용", required = true)
     @NotBlank(message = "사용자 이름이 필요합니다.")
     private String username;
+    @ApiModelProperty(notes = "사용자 이메일", example = "test@test.com", required = true)
     @NotBlank(message = "이메일이 필요합니다.")
     @Email(message = "잘못된 이메일 입니다.")
     private String email;
+    @ApiModelProperty(notes = "사용자 비밀번호", example = "password", required = true)
     @NotBlank(message = "비밀번호가 필요합니다.")
     private String password;
 }
