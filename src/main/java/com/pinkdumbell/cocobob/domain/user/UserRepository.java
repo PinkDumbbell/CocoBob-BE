95b70f8bd81abc126e06346d96e4396935532a2f
ErranderLee <dre12am14@gmail.com>
2022-07-17 15:38:38 +0900

    # CB-91 feat: find user by user's email # ################ - create method to find user by user's email ################ - 이메일을 통해 사용자를 찾는 메서드를 레포지토리에 추가.   - 이메일 중복 체크를 하기 위함.

--- src/main/java/com/pinkdumbell/cocobob/domain/user/UserRepository.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/user/UserRepository.java
@@ -2,5 +2,8 @@ package com.pinkdumbell.cocobob.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
