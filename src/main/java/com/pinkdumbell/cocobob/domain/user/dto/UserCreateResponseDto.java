fb191cd56dfed75bd601d6eaa9178605be25ed6c
ErranderLee <dre12am14@gmail.com>
2022-07-17 15:52:08 +0900

    # CB-91 feat: add constructor to UserCreateResponseDto with User entity # ################ - add constructor to UserCreateResponseDto with User entity ################ - User entity를 통해 UserCreateResponseDto를 만들기 위해 생성자 추가.

--- src/main/java/com/pinkdumbell/cocobob/domain/user/dto/UserCreateResponseDto.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/user/dto/UserCreateResponseDto.java
@@ -1,4 +1,16 @@
 package com.pinkdumbell.cocobob.domain.user.dto;
 
+import com.pinkdumbell.cocobob.domain.user.User;
+import lombok.Getter;
+
+@Getter
 public class UserCreateResponseDto {
+    private final Long userId;
+    private final String email;
+    private final String username;
+    public UserCreateResponseDto(User entity) {
+        this.userId = entity.getId();
+        this.email = entity.getEmail();
+        this.username = entity.getUsername();
+    }
 }
