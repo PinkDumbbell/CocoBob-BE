9f60e910a6258a88958d89965bd8f4d6f7250c0d
ErranderLee <dre12am14@gmail.com>
2022-07-17 15:53:19 +0900

    # CB-91 feat: add signup method to UserService # ################ - add signup method to UserService ################ - UserService에 회원가입 메서드를 추가함. 중복된 이메일이 있으면 예외를 던진다.

--- src/main/java/com/pinkdumbell/cocobob/domain/user/UserService.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/user/UserService.java
@@ -1,9 +1,17 @@
 package com.pinkdumbell.cocobob.domain.user;
 
+import com.pinkdumbell.cocobob.domain.user.dto.UserCreateRequestDto;
+import com.pinkdumbell.cocobob.domain.user.dto.UserCreateResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserCreateResponseDto signup(UserCreateRequestDto requestDto) {
+        return new UserCreateResponseDto(userRepository.save(userRepository.findByEmail(requestDto.getEmail())
+                .orElseThrow(() -> new RuntimeException("해당 이메일을 가진 사용자가 존재합니다."))));
+    }
 }
