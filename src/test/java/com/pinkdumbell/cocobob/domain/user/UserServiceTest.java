436ec9adc093343a32e95fa442e66d9089141713
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-07-18 17:32:21 +0900

    CB-20 [chore]: add dependency into test code
    테스트 코드에 의존적인 값들과 객체를 생성하여 테스트과 원활히 되도록 하였습니다.

--- src/test/java/com/pinkdumbell/cocobob/domain/user/UserServiceTest.java
+++ src/test/java/com/pinkdumbell/cocobob/domain/user/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.pinkdumbell.cocobob.domain.user;
 
+import com.pinkdumbell.cocobob.domain.auth.JwtTokenProvider;
 import com.pinkdumbell.cocobob.domain.user.dto.UserCreateRequestDto;
 import com.pinkdumbell.cocobob.domain.user.dto.UserCreateResponseDto;
 import org.assertj.core.api.Assertions;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Transactional
 @SpringBootTest
@@ -17,6 +19,12 @@ class UserServiceTest {
     UserRepository userRepository;
     @Autowired
     UserService userService;
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Test
     @DisplayName("주어진 사용자 정보를 통해 회원가입을 할 수 있다.")
