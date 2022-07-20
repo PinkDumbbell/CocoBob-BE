d59cb0e44a9d99545179b94ac9ce4ca97100d172
ErranderLee <dre12am14@gmail.com>
2022-07-20 17:28:29 +0900

    # CB-158 feat : create entity # # ################ - create Review entity ################ - Review 엔티티 생성 및 Product, User 엔티티와 연관관계 설정

---
+++ src/main/java/com/pinkdumbell/cocobob/domain/product/review/Review.java
@@ -0,0 +1,29 @@
+package com.pinkdumbell.cocobob.domain.product.review;
+
+import com.pinkdumbell.cocobob.domain.product.Product;
+import com.pinkdumbell.cocobob.domain.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
