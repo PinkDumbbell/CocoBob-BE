177b5f16278b40d6fe3cfa88077d201e4a975db4
ErranderLee <dre12am14@gmail.com>
2022-07-21 20:55:33 +0900

    CB-128 feat : create PetController # # ################ - 반려동물에 대한 컨트롤러 생성 및 반려동물 등록 메서드 생성

---
+++ src/main/java/com/pinkdumbell/cocobob/domain/pet/PetController.java
@@ -0,0 +1,23 @@
+package com.pinkdumbell.cocobob.domain.pet;
+
+import com.pinkdumbell.cocobob.domain.pet.dto.PetCreateRequestDto;
+import com.pinkdumbell.cocobob.domain.pet.dto.PetCreateResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("v1/pets")
+@RequiredArgsConstructor
+@RestController
+public class PetController {
+
+    private final PetService petService;
+
+    @PostMapping("")
+    public ResponseEntity<PetCreateResponseDto> register(@ModelAttribute PetCreateRequestDto requestDto) {
+        return ResponseEntity.ok(petService.register(requestDto));
+    }
+}
