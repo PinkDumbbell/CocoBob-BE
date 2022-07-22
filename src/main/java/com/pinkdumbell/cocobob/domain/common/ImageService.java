568b2dff80222d3f1c7c815980f6d5e96e8753f2
ErranderLee <dre12am14@gmail.com>
2022-07-22 18:58:38 +0900

    CB-128 feat : create ImageService # # ################ - 이미지 저장과 리사이징을 위한 ImageService 생성

---
+++ src/main/java/com/pinkdumbell/cocobob/domain/common/ImageService.java
@@ -0,0 +1,76 @@
+package com.pinkdumbell.cocobob.domain.common;
+
+import lombok.RequiredArgsConstructor;
+import marvin.image.MarvinImage;
+import org.marvinproject.image.transform.scale.Scale;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Service
+public class ImageService {
+
+    private final AwsS3Service awsS3Service;
+
+    public String saveImage(MultipartFile image) {
+        isImage(image);
+        return awsS3Service.saveImage(image);
+    }
+
+    private void isImage(MultipartFile image) {
+        if(!image.getContentType().startsWith("image")) {
+            throw new RuntimeException("not image");
+        }
+    }
+
+    public MultipartFile resizeImage(MultipartFile originalImage, int targetWidth) {
+        try {
+            BufferedImage bufferedImage = ImageIO.read(originalImage.getInputStream());
+            if (bufferedImage.getWidth() < targetWidth) {
+                return originalImage;
+            }
+            MarvinImage marvinImage = processResizing(bufferedImage, targetWidth);
+            return new MockMultipartFile(originalImage.getOriginalFilename(),
+                    createResizedImageName(originalImage.getOriginalFilename()),
+                    originalImage.getContentType(),
+                    writeResizedImage(marvinImage, originalImage.getContentType()).toByteArray());
+        } catch (IOException e) {
+            throw new RuntimeException("이미지 리사이징 실패");
+        }
+    }
+
+    private MarvinImage processResizing(BufferedImage bufferedImage, int targetWidth) {
+        MarvinImage marvinImage = new MarvinImage(bufferedImage);
+        Scale scale = new Scale();
+        scale.load();
+        scale.setAttribute("newWidth", targetWidth);
+        scale.setAttribute("newHeight", targetWidth * bufferedImage.getHeight() / bufferedImage.getWidth());
+        scale.process(marvinImage.clone(), marvinImage, null, null, false);
+
+        return marvinImage;
+    }
+
+    private ByteArrayOutputStream writeResizedImage(MarvinImage marvinImage, String contentType) {
+        try {
+            BufferedImage bufferedImageNoAlpha = marvinImage.getBufferedImageNoAlpha();
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImageNoAlpha,
+                    contentType.substring(contentType.lastIndexOf("/") + 1),
+                    byteArrayOutputStream);
+            byteArrayOutputStream.flush();
+
+            return byteArrayOutputStream;
+        } catch (IOException e) {
+            throw new RuntimeException("이미지 리사이징 실패");
+        }
+    }
+    private String createResizedImageName(String originalImageName) {
+        return "resized_".concat(originalImageName);
+    }
+}
