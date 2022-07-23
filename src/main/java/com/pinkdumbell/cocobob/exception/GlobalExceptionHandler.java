52456fe002da5fa24c8960583cdc08402d3d55eb
ErranderLee <dre12am14@gmail.com>
2022-07-23 17:25:39 +0900

    CB-128 feat : 반려동물 등록 시 정보를 입력받을 때 해당 정보들을 validation 시 발생하는 예외를 다루는 메서드를 생성 # # ################ - [CB-189] [CB-190] 반려동물 등록 시 정보를 입력받을 때 해당 정보들을 validation 시 발생하는 예외를 다루는 메서드를 생성

--- src/main/java/com/pinkdumbell/cocobob/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pinkdumbell/cocobob/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.pinkdumbell.cocobob.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,6 +18,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error(e.getMessage(), e);
+        String message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return ErrorResponse.toResponseEntity(ErrorCode.BAD_REQUEST, message);
+    }
+
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
         log.error(e.getMessage(), e);
         String message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
         return ErrorResponse.toResponseEntity(ErrorCode.BAD_REQUEST, message);
