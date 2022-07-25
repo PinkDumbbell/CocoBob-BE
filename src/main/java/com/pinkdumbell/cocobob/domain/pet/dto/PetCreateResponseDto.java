2163d9d64759834ff89d36c28c9c9633682d944a
ErranderLee <dre12am14@gmail.com>
2022-07-25 17:40:22 +0900

    CB-128 chore : swagger을 통한 api 명세 작성 # # ################ - swagger를 통하여 반려동물 등록 api 명세 작성 및 dto 명세 작성

--- src/main/java/com/pinkdumbell/cocobob/domain/pet/dto/PetCreateResponseDto.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/pet/dto/PetCreateResponseDto.java
@@ -1,10 +1,12 @@
 package com.pinkdumbell.cocobob.domain.pet.dto;
 
 import com.pinkdumbell.cocobob.domain.pet.Pet;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 @Getter
 public class PetCreateResponseDto {
+    @ApiModelProperty(notes = "반려동물 아이디", example = "3")
     private Long petId;
 
     public PetCreateResponseDto(Pet entity) {
