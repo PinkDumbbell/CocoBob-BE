7b46b9d8f8b784184b53de90b4d2827db80f2976
ErranderLee <dre12am14@gmail.com>
2022-07-27 12:56:58 +0900

    [CB-213] hotfix : cors 설정 문제 해결 # # ################ - cors 필터를 추가함으로써 문제를 해결. [CB-213]에 해결방법과 추측된 원이에 대하여 자세하게 정리해두었음.

--- src/main/java/com/pinkdumbell/cocobob/config/SecurityConfig.java
+++ src/main/java/com/pinkdumbell/cocobob/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable() // rest api이므로 기본설정 미사용
+                .cors().and()
             .csrf().disable() // rest api이므로 csrf 보안 미사용
             .formLogin().disable().sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt로 인증하므로 세션 미사용
