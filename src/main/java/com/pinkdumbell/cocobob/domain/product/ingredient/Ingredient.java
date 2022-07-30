becacbc01c349670577c5166c86ebf4a23df786c
ErranderLee <dre12am14@gmail.com>
2022-07-30 16:43:14 +0900

    [CB-226] feat : 상품 주재료 엔티티 작성 # # ################ [CB-229] - 사료 주재료 데이터를 담을 엔티티 작성

--- src/main/java/com/pinkdumbell/cocobob/domain/product/ingredient/Ingredient.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/product/ingredient/Ingredient.java
@@ -1,4 +1,49 @@
 package com.pinkdumbell.cocobob.domain.product.ingredient;
 
+import com.pinkdumbell.cocobob.domain.product.Product;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ingredient_id")
+    private Long id;
+
+    private Boolean beef;
+
+    private Boolean mutton;
+
+    private Boolean chicken;
+
+    private Boolean duck;
+
+    private Boolean turkey;
+
+    private Boolean meat;
+
+    private Boolean salmon;
+
+    private Boolean hydrolyticBeef;
+
+    private Boolean hydrolyticMutton;
+
+    private Boolean hydrolyticChicken;
+
+    private Boolean hydrolyticDuck;
+
+    private Boolean hydrolyticTurkey;
+
+    private Boolean hydrolyticMeat;
+
+    private Boolean hydrolyticSalmon;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
