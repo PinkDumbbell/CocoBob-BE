df59aa03dac76055773ed14f39d4ab7ea7434a7a
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-08-01 17:55:34 +0900

    CB-226 [refactor]: remove redundant variable at productDetail
    productDetail 메소드에서 불필요한 변수 삭제 후 결과 값 바로 리턴하게 하였습니다.

--- src/main/java/com/pinkdumbell/cocobob/domain/product/ProductController.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/product/ProductController.java
@@ -49,11 +49,9 @@ public class ProductController {
     @GetMapping("/{product_id}")
     public ResponseEntity<ProductDetailResponseClass> productDetail(@PathVariable Long product_id) {
 
-        ProductDetailResponseDto productDetail = productService.findProductDetailById(product_id);
-
         return ResponseEntity.ok(
             new ProductDetailResponseClass(HttpStatus.OK.value(), "SUCCESS LOAD PROUDCT",
-                "상품 가져오기 성공", productDetail));
+                "상품 가져오기 성공", productService.findProductDetailById(product_id)));
     }
 
 }
