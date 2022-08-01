c8deb0d318fe80d75dd47f46afd2192225c26b0d
ErranderLee <dre12am14@gmail.com>
2022-08-01 14:06:55 +0900

    [CB-257] feat : 사용자 정보를 조회하는 쿼리 작성
    ################
    - 사용자 정보를 조회할 때 join fetch를 통해 한 번에 반려동물 정보를 가져올 수 있도록 작성

--- src/main/java/com/pinkdumbell/cocobob/domain/user/UserRepository.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/user/UserRepository.java
@@ -1,10 +1,15 @@
 package com.pinkdumbell.cocobob.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    @Query("select distinct u from User u left join fetch u.pets where u.email = :email")
+    Optional<User> findUserByEmailWithPet(@Param("email") String email);
 }
