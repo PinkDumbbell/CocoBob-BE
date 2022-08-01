ef49c50d1038eea6463a264da2dfe4c90473af4d
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-08-01 18:26:05 +0900

    CB-226 [refactor]: make constructor at ProductDetailResponseDto 빌더 패턴으로 사용을 dto를 생성하여 리턴하다보니 너무나도 많은 프로퍼티에 의해 가독성이 부족하였습니다. 이를 생성자를 생성함으로 난잡한 코드를 개선하였습니다.

--- src/main/java/com/pinkdumbell/cocobob/domain/product/ProductService.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/product/ProductService.java
@@ -41,43 +41,7 @@ public class ProductService {
             .map(PetPropertyResponseDto::new).collect(
                 Collectors.toList());
 
-        return ProductDetailResponseDto.builder()
-            .productId(foundProduct.getId())
-            .code(foundProduct.getCode())
-            .name(foundProduct.getName())
-            .price(foundProduct.getPrice())
-            .productImage(foundProduct.getProductImage())
-            .productDetailImage(foundProduct.getProductDetailImage())
-            .description(foundProduct.getDescription())
-            .protein(foundProduct.getProtein())
-            .amountOfProteinPerMcal(foundProduct.getAmountOfProteinPerMcal())
-            .fat(foundProduct.getFat())
-            .amountOfFatPerMcal(foundProduct.getAmountOfFatPerMcal())
-            .fiber(foundProduct.getFiber())
-            .amountOfFiberPerMcal(foundProduct.getAmountOfFiberPerMcal())
-            .mineral(foundProduct.getMineral())
-            .amountOfMineralPerMcal(foundProduct.getAmountOfMineralPerMcal())
-            .calcium(foundProduct.getCalcium())
-            .amountOfCalciumPerMcal(foundProduct.getAmountOfCalciumPerMcal())
-            .phosphorus(foundProduct.getPhosphorus())
-            .amountOfPhosphorusPerMcal(foundProduct.getAmountOfPhosphorusPerMcal())
-            .moisture(foundProduct.getMoisture())
-            .kcalPerKg(foundProduct.getKcalPerKg())
-            .isAAFCOSatisfied(foundProduct.getIsAAFCOSatisfied())
-            .beef(foundProduct.getBeef())
-            .mutton(foundProduct.getMutton())
-            .duck(foundProduct.getDuck())
-            .turkey(foundProduct.getTurkey())
-            .meat(foundProduct.getMeat())
-            .salmon(foundProduct.getSalmon())
-            .hydrolyticBeef(foundProduct.getHydrolyticBeef())
-            .hydrolyticMutton(foundProduct.getHydrolyticMutton())
-            .hydrolyticChicken(foundProduct.getHydrolyticChicken())
-            .hydrolyticTurkey(foundProduct.getHydrolyticTurkey())
-            .hydrolyticMeat(foundProduct.getHydrolyticMeat())
-            .hydrolyticSalmon(foundProduct.getHydrolyticSalmon())
-            .petProperties(allProperty)
-            .build();
+        return new ProductDetailResponseDto(foundProduct, allProperty);
     }
 
 
