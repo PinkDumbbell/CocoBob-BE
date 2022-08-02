0f25a240255ac2cd2dedcbdd427349c54a8c4d10
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-08-02 13:36:31 +0900

    CB-226 [test]: add dummy data for test & add ProductRepositoryTest
    데이터 조회가 정상적으로 이루어지는지 확인하기 위해서 리포지토리 테스트를 진행하였습니다.
    실제 db와 연동이 되어 있지 않으므로 beforeEach로 더미 데이터를 생성해서 저장하여 테스트 하엿습니다.

---
+++ src/test/java/com/pinkdumbell/cocobob/domain/product/ProductRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.pinkdumbell.cocobob.domain.product;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
+
+@DataJpaTest
+@Transactional
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    void init() {
+        for (int dummy_id = 1; dummy_id < 30; dummy_id++) {
+            Product dummy_product = new Product();
+            productRepository.save(dummy_product);
+        }
+    }
+
+    @Test
+    @DisplayName("사용자가 설정한 페이지 사이즈, 페이지 넘버, 정렬 기준으로 전체 상품을 페이지로 가져올 수 있다.")
+    void findProductAll() {
+
+        PageRequest pageRequest = PageRequest.of(5, 10, Sort.by("id").descending());
+
+        Page<Product> result = productRepository.findAll(pageRequest);
+
+        Assertions.assertThat(result.getNumber()).isEqualTo(pageRequest.getPageNumber());
+        Assertions.assertThat(result.getSize()).isEqualTo(pageRequest.getPageSize());
+    }
+
+    @Test
+    @DisplayName("사용자가 요청한 상품 코드로 상품의 상세 정보 조회가 가능해야 한다.")
+    void findProductDetailById() {
+
+        //ProductId 10을 가지고 있는 상품 조회
+        Product result = productRepository.findById(10L).get();
+
+        Assertions.assertThat(result.getId()).isEqualTo(10L);
+    }
+}
