9bd41a2b8d70956a7fb33eb0a5f80bab051640b7
ErranderLee <dre12am14@gmail.com>
2022-08-03 00:06:14 +0900

    [CB-266] test : ProperBirthInfo 어노테이션 테스트
    ################
    - ProperBirthInfo 어노테이션 테스트
      - 1. age가 없을 때
      - 2. age 중 birthday만 있을 때
      - 3. birthday가 없는데 months가 0일 때

--- src/test/java/com/pinkdumbell/cocobob/domain/pet/PetControllerTest.java
+++ src/test/java/com/pinkdumbell/cocobob/domain/pet/PetControllerTest.java
@@ -2,6 +2,7 @@ package com.pinkdumbell.cocobob.domain.pet;
 
 
 import com.pinkdumbell.cocobob.domain.auth.JwtTokenProvider;
+import com.pinkdumbell.cocobob.domain.pet.dto.PetCreateRequestAgeDto;
 import com.pinkdumbell.cocobob.domain.pet.dto.PetCreateRequestDto;
 import com.pinkdumbell.cocobob.domain.pet.dto.PetCreateResponseDto;
 import com.pinkdumbell.cocobob.domain.user.dto.LoginUserInfo;
@@ -79,19 +80,19 @@ class PetControllerTest {
     void testValidationForPetCreateRequestDtoWithInvalidValue() throws Exception {
         LocalDate birthday = LocalDate.parse("3000-12-31");
         Float bodyWeight = -1.4F;
-        Integer age = 0;
+        Integer months = 0;
         Integer activityLevel = 10;
         Long breedId = -1L;
 
         MvcResult result = mvc.perform(post("/v1/pets")
                         .contentType(MediaType.MULTIPART_FORM_DATA)
-                        .param("birthday", String.valueOf(birthday))
-                        .param("age", String.valueOf(age))
+                        .param("age.birthday", String.valueOf(birthday))
+                        .param("age.months", String.valueOf(months))
                         .param("bodyWeight", String.valueOf(bodyWeight))
                         .param("activityLevel", String.valueOf(activityLevel))
                         .param("breedId", String.valueOf(breedId))
                         .flashAttr("PetCreateRequestDto", PetCreateRequestDto.builder()
-                                .birthday(birthday)
+                                .age(new PetCreateRequestAgeDto(months, birthday))
                                 .bodyWeight(bodyWeight)
                                 .activityLevel(activityLevel)
                                 .breedId(breedId)
@@ -102,7 +103,6 @@ class PetControllerTest {
         Assertions.assertThat(result.getResponse().getStatus()).isEqualTo(400);
         Assertions.assertThat(result.getResponse().getContentAsString())
                 .contains(Arrays.asList(
-                        "나이(개월 수)는 0보다 커야합니다.",
                         "생일은 과거 또는 현재의 날짜여야 합니다.",
                         "몸무게는 0보다 커야 합니다.",
                         "활동 수준은 1~5의 값이어야 합니다.",
@@ -112,10 +112,58 @@ class PetControllerTest {
 
     @Test
     @WithMockUser("USER")
+    @DisplayName("ProperBirthInfo 어노테이션 테스트 : age 정보 없을 때")
+    void testProperBirthInfoWithNoAge() throws Exception {
+        MvcResult result = mvc.perform(post("/v1/pets")
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                        .flashAttr("PetCreateRequestDto", PetCreateRequestDto.builder()
+                                .build())
+                ).andReturn();
+
+        Assertions.assertThat(result.getResponse().getContentAsString())
+                .contains("반려동물의 나이(개월 수)가 필요합니다.");
+    }
+
+    @Test
+    @WithMockUser("USER")
+    @DisplayName("ProperBirthInfo 어노테이션 테스트 : 생일만 있을 때")
+    void testProperBirthInfoWithBirthday() throws Exception {
+        LocalDate birthday = LocalDate.parse("2020-12-31");
+
+        MvcResult result = mvc.perform(post("/v1/pets")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .param("age.birthday", String.valueOf(birthday))
+                .flashAttr("PetCreateRequestDto", PetCreateRequestDto.builder()
+                        .build())
+        ).andReturn();
+
+        Assertions.assertThat(result.getResponse().getContentAsString())
+                .contains("반려동물의 나이(개월 수)가 필요합니다.");
+    }
+
+    @Test
+    @WithMockUser("USER")
+    @DisplayName("ProperBirthInfo 어노테이션 테스트 : 생일 없이 개월 수가 0일 때")
+    void testProperBirthInfoWithZeroAgeWithoutBirthday() throws Exception {
+        Integer months = 0;
+
+        MvcResult result = mvc.perform(post("/v1/pets")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .param("age.months", String.valueOf(months))
+                .flashAttr("PetCreateRequestDto", PetCreateRequestDto.builder()
+                        .build())
+        ).andReturn();
+
+        Assertions.assertThat(result.getResponse().getContentAsString())
+                .contains("반려동물의 생일과 나이(개월 수)를 확인해주세요.");
+    }
+
+    @Test
+    @WithMockUser("USER")
     @DisplayName("올바른 값이 요청을 통해 들어왔을 때 컨트롤러의 반환값을 테스트")
     void testReturnOfControllerWithValidInputValues() throws Exception {
         Float bodyWeight = 5.4F;
-        Integer age = 20;
+        Integer months = 20;
         Integer activityLevel = 3;
         Long breedId = 1L;
         Boolean isSpayed = true;
@@ -136,7 +184,7 @@ class PetControllerTest {
                         .param("isSpayed", String.valueOf(isSpayed))
                         .param("breedId", String.valueOf(breedId))
                         .param("activityLevel", String.valueOf(activityLevel))
-                        .param("age", String.valueOf(age))
+                        .param("age.months", String.valueOf(months))
                         .param("bodyWeight", String.valueOf(bodyWeight))
                 )
                 .andExpect(jsonPath("$.data.petId").value(1L))
