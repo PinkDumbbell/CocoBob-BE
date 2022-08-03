23944f7379498955948d0405c62bc58336c5566f
ErranderLee <dre12am14@gmail.com>
2022-08-03 18:14:31 +0900

    [CB-173] feat : 사용자 아이디를 통해 반려동물을 조회하는 레포지토리 메서드 생성
    ################
    - [CB-274] fetch join을 활용하여 pet과 breed를 한 번에 조회할 수 있도록 함

--- src/main/java/com/pinkdumbell/cocobob/domain/pet/PetRepository.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/pet/PetRepository.java
@@ -1,6 +1,12 @@
 package com.pinkdumbell.cocobob.domain.pet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
+    @Query("select p from Pet p join fetch p.breed where p.user.id = :userId")
+    List<Pet> findAllByUserIdWithBreed(@Param("userId") Long userId);
 }
