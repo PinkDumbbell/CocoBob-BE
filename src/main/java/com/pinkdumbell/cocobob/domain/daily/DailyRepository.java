350334e9085f187443ac22bcfde2b7dfef014cab
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-08-08 21:48:05 +0900

    CB-283 [feat]: generate Daily JPA method
    데일리 기록을 조회 하기 위한 메소드 정의

--- src/main/java/com/pinkdumbell/cocobob/domain/daily/DailyRepository.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/daily/DailyRepository.java
@@ -1,7 +1,9 @@
 package com.pinkdumbell.cocobob.domain.daily;
 
+import com.pinkdumbell.cocobob.domain.pet.Pet;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DailyRepository extends JpaRepository<Daily,Long> {
-
+    List<Daily> findAllByPet(Pet pet);
 }
--- src/main/java/com/pinkdumbell/cocobob/domain/daily/image/DailyImageRepository.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/daily/image/DailyImageRepository.java
@@ -1,7 +1,10 @@
 package com.pinkdumbell.cocobob.domain.daily.image;
 
+import com.pinkdumbell.cocobob.domain.daily.Daily;
+import com.pinkdumbell.cocobob.domain.pet.Pet;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DailyImageRepository extends JpaRepository<DailyImage,Long> {
-
+    List<DailyImage> findAllByDaily(Daily daily);
 }
