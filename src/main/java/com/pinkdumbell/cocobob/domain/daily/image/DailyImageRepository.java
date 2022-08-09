f41b2211d00e62aa684660be0c317ef45345563b
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-08-09 15:42:39 +0900

    CB-283 [feat]: count daily image
    한 기록에 포함되어 있는 이미지 갯수를 구하는 JPA 작성

--- src/main/java/com/pinkdumbell/cocobob/domain/daily/image/DailyImageRepository.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/daily/image/DailyImageRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DailyImageRepository extends JpaRepository<DailyImage,Long> {
     List<DailyImage> findAllByDaily(Daily daily);
+    Long countAllByDaily(Daily daily);
 }
