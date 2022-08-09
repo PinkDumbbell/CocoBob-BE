5cab2b02815dc95349667cff474f088012903029
ErranderLee <dre12am14@gmail.com>
2022-08-09 14:21:00 +0900

    [CB-173] [CB-299] fix : 테스트 오류 수정
    ################
    - 테스트 시 빈 생성할 때 구글 로그인 정보에 대한 값을 주입할 수 있도록 수정

--- src/test/java/com/pinkdumbell/cocobob/CocobobApplicationTests.java
+++ src/test/java/com/pinkdumbell/cocobob/CocobobApplicationTests.java
@@ -3,6 +3,7 @@ package com.pinkdumbell.cocobob;
 import com.pinkdumbell.cocobob.common.EmailUtil;
 import com.pinkdumbell.cocobob.config.MailConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -11,9 +12,19 @@ class CocobobApplicationTests {
 
 	@MockBean
 	EmailUtil emailUtil;
-
 	@MockBean
 	MailConfig mailConfig;
+	@Value("${google.auth.url}")
+	private String googleAuthUrl;
+	@Value("${google.login.url}")
+	private String googleLoginUrl;
+	@Value("${google.client.id}")
+	private String googleClientId;
+	@Value("${google.client.secret}")
+	private String googleClientSecret;
+	@Value("${google.redirect.url}")
+	private String googleRedirectUrl;
+
 	@Test
 	void contextLoads() {
 	}
