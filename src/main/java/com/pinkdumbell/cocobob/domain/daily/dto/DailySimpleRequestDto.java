f29f87ca5832e4c94e33fff9393fa2feca957355
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-08-10 15:25:37 +0900

    CB-305 [docs]:write swagger about DailySimpleDto

--- src/main/java/com/pinkdumbell/cocobob/domain/daily/dto/DailySimpleRequestDto.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/daily/dto/DailySimpleRequestDto.java
@@ -1,5 +1,6 @@
 package com.pinkdumbell.cocobob.domain.daily.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import java.time.YearMonth;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -14,8 +15,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Builder
 public class DailySimpleRequestDto {
 
+    @ApiModelProperty(notes = "찾고 싶은 연월", example = "2022-08")
     @DateTimeFormat(pattern = "yyyy-MM")
     @NotNull(message = "필수 입력 항목(년 월)이 없습니다.")
     private YearMonth yearMonth;
-
 }
--- src/main/java/com/pinkdumbell/cocobob/domain/daily/dto/DailySimpleResponseDto.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/daily/dto/DailySimpleResponseDto.java
@@ -2,6 +2,7 @@ package com.pinkdumbell.cocobob.domain.daily.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pinkdumbell.cocobob.domain.daily.Daily;
+import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,8 +13,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public class DailySimpleResponseDto {
 
+    @ApiModelProperty(notes = "기록된 날짜", example = "2022-08-01")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private final LocalDate date;
+    @ApiModelProperty(notes = "daily id", example = "데일리 기록 id")
     private final Long dailyId;
 
     public DailySimpleResponseDto(Daily daily) {
