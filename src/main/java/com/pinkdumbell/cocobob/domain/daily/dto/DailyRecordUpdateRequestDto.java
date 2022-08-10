f1e82415147e09d0df29a360f13051ec546d3702
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-08-10 15:18:19 +0900

    CB-305 [docs]: write swagger DailyRecordUpdateDto

--- src/main/java/com/pinkdumbell/cocobob/domain/daily/dto/DailyRecordUpdateRequestDto.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/daily/dto/DailyRecordUpdateRequestDto.java
@@ -1,5 +1,6 @@
 package com.pinkdumbell.cocobob.domain.daily.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import lombok.AllArgsConstructor;
 
@@ -11,10 +12,16 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DailyRecordUpdateRequestDto {
+    @ApiModelProperty(notes = "추가 업로드할 이미지", example = "1.png, 2.png...")
     private List<MultipartFile> images;
+    @ApiModelProperty(notes = "데일리 기록", example = "건강하개 오늘은 맑음이냥~")
     private String note;
+    @ApiModelProperty(notes = "급여 사료량", example = "430")
     private Integer feedAmount;
+    @ApiModelProperty(notes = "총 산책시간", example = "3")
     private Integer walkTotalTime;
+    @ApiModelProperty(notes = "산책 거리", example = "2.5")
     private Float walkDistance;
+    @ApiModelProperty(notes = "산책 GPS", example = "[...]")
     private String walkGps;
 }
