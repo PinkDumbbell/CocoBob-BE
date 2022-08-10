bb18bf6debfd037e0d187912e30935494b0dd2ad
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-08-10 15:12:38 +0900

    CB-305 [docs]: write swagger about DailyRecordGetRequestDto&ResponseDto

--- src/main/java/com/pinkdumbell/cocobob/domain/daily/dto/DailyRecordGetRequestDto.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/daily/dto/DailyRecordGetRequestDto.java
@@ -1,5 +1,6 @@
 package com.pinkdumbell.cocobob.domain.daily.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -14,10 +15,12 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Builder
 public class DailyRecordGetRequestDto {
 
+    @ApiModelProperty(notes = "시작 날짜", example = "2022-01-01")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @NotNull(message = "필수 입력 항목(시작 날짜)가 없습니다.")
     private LocalDate startDate;
 
+    @ApiModelProperty(notes = "종료 날짜", example = "2022-12-31")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @NotNull(message = "필수 입력 항목(종료 날짜)가 없습니다.")
     private LocalDate lastDate;
--- src/main/java/com/pinkdumbell/cocobob/domain/daily/dto/DailyRecordGetResponseDto.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/daily/dto/DailyRecordGetResponseDto.java
@@ -3,6 +3,7 @@ package com.pinkdumbell.cocobob.domain.daily.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pinkdumbell.cocobob.domain.daily.Daily;
 import com.pinkdumbell.cocobob.domain.daily.image.DailyImage;
+import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,27 +14,29 @@ import lombok.Getter;
 @AllArgsConstructor
 public class DailyRecordGetResponseDto {
 
+    @ApiModelProperty(notes = "daily Id", example = "1")
     private final Long id;
-
+    @ApiModelProperty(notes = "기록 날짜", example = "2022-08-15")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private final LocalDate date;
-
+    @ApiModelProperty(notes = "급여 사료량", example = "530")
     private final Integer feedAmount;
-
+    @ApiModelProperty(notes = "총 산책시간", example = "3")
     private final Integer walkTotalTime;
-
+    @ApiModelProperty(notes = "산책 거리", example = "2.5")
     private final Float walkDistance;
-
+    @ApiModelProperty(notes = "산책 GPS", example = "[...]")
     private final String walkGps;
-
+    @ApiModelProperty(notes = "데일리 기록", example = "코코가 오늘은 밥을 맛있게 먹어 기특했다.")
     private final String note;
-
+    @ApiModelProperty(notes = "이미지들", example = "{}")
     private final List<DailyImageInfo> images = new ArrayList<>();
 
     @Getter
     private static class DailyImageInfo {
-
+        @ApiModelProperty(notes = "이미지 경로", example = "이미지 URL")
         private final String imagePath;
+        @ApiModelProperty(notes = "daily image id", example = "데일리 이미지 Id")
         private final Long dailyImageId;
 
         public DailyImageInfo(DailyImage dailyImage) {
