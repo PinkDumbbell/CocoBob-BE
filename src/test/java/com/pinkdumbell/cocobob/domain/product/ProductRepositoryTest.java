de24ddbafcb118fa8e259617b4904fbe6ccd3b30
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-08-31 18:17:55 +0900

    CB-370 [test]: write test code for product
    키워드 입력시 연관된 상품 목록을 제공하는 API에 대한 테스트를 진행하였습니다.

--- src/test/java/com/pinkdumbell/cocobob/domain/product/ProductRepositoryTest.java
+++ src/test/java/com/pinkdumbell/cocobob/domain/product/ProductRepositoryTest.java
@@ -1,6 +1,10 @@
 package com.pinkdumbell.cocobob.domain.product;
 
 
+import com.pinkdumbell.cocobob.config.TestConfig;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -8,13 +12,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
 @Transactional
+@Import(TestConfig.class)
 class ProductRepositoryTest {
 
     @Autowired
@@ -22,6 +25,9 @@ class ProductRepositoryTest {
 
     @MockBean
     ProductSearchQueryDslImpl productSearchQueryDsl;
+
+    @Autowired
+    JPAQueryFactory jpaQueryFactory;
 
     @BeforeEach
     void init() {
@@ -33,12 +39,24 @@ class ProductRepositoryTest {
 
     @Test
     @DisplayName("사용자가 요청한 상품 코드로 상품의 상세 정보 조회가 가능해야 한다.")
-    void
-    findProductDetailById() {
+    void findProductDetailById() {
 
         //ProductId 10을 가지고 있는 상품 조회
         Product result = productRepository.findById(10L).get();
 
         Assertions.assertThat(result.getId()).isEqualTo(10L);
     }
+
+    @Test
+    @DisplayName("keyword를 통한 연관 상품 정보를 제공할 수 있어야 한다.")
+    void provideProductListByKeyword() {
+        QProduct qProduct = QProduct.product;
+
+        List<Tuple> result = jpaQueryFactory.select(qProduct.brand, qProduct.name)
+            .from(qProduct)
+            .where(ProductPredicate.makeKeywordBooleanBuilder("test"))
+            .fetch();
+        Assertions.assertThat(result).isNotNull();
+    }
+
 }
