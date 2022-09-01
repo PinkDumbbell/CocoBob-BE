2eddf0165bfc20b3086608d993c5e4fdc4a0650b
ErranderLee <dre12am14@gmail.com>
2022-09-01 17:36:25 +0900

    [CB-368] refactor : daily 엔티티 불필요 필드 삭제
    ################
    - daily 엔티티에서 불필요한 필드를 삭제함

--- src/main/java/com/pinkdumbell/cocobob/domain/daily/Daily.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/daily/Daily.java
@@ -2,8 +2,6 @@ package com.pinkdumbell.cocobob.domain.daily;
 
 import com.pinkdumbell.cocobob.common.BaseEntity;
 import com.pinkdumbell.cocobob.domain.daily.dto.DailyRecordRegisterRequestDto;
-import com.pinkdumbell.cocobob.domain.daily.dto.DailyRecordUpdateRequestDto;
-import com.pinkdumbell.cocobob.domain.daily.dto.TempDailyRequestDto;
 import com.pinkdumbell.cocobob.domain.pet.Pet;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,51 +25,10 @@ public class Daily extends BaseEntity {
 
     private LocalDate date;
 
-    private Integer feedAmount;
-
-    private Integer walkTotalTime;
-
-    private Float walkDistance;
-
-    @Column(columnDefinition = "TEXT")
-    private String walkGps;
-
     @Column(columnDefinition = "TEXT")
     private String note;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "pet_id")
     private Pet pet;
-
-    @Column(columnDefinition = "TEXT")
-    private String data;
-
-    public Daily(DailyRecordRegisterRequestDto dailyRecordRegisterRequestDto, Pet pet) {
-        this.date = dailyRecordRegisterRequestDto.getDate();
-        this.feedAmount = dailyRecordRegisterRequestDto.getFeedAmount();
-        this.walkTotalTime = dailyRecordRegisterRequestDto.getWalkTotalTime();
-        this.walkDistance = dailyRecordRegisterRequestDto.getWalkDistance();
-        this.walkGps = dailyRecordRegisterRequestDto.getWalkGps();
-        this.note = dailyRecordRegisterRequestDto.getNote();
-        this.pet = pet;
-    }
-
-    public Daily(TempDailyRequestDto requestDto, Pet pet) {
-        this.date = requestDto.getDate();
-        this.data = requestDto.getData();
-        this.pet = pet;
-    }
-
-    public void tempUpdateDaily(TempDailyRequestDto requestDto) {
-        this.data = requestDto.getData();
-    }
-
-    public void updateDaily(DailyRecordUpdateRequestDto dailyRecordUpdateRequestDto) {
-
-        this.feedAmount = dailyRecordUpdateRequestDto.getFeedAmount();
-        this.note = dailyRecordUpdateRequestDto.getNote();
-        this.walkTotalTime = dailyRecordUpdateRequestDto.getWalkTotalTime();
-        this.walkDistance = dailyRecordUpdateRequestDto.getWalkDistance();
-        this.walkGps = dailyRecordUpdateRequestDto.getWalkGps();
-    }
 }
