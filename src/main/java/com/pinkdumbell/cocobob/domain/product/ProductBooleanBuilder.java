651878300d5a60357c57f69a69297f0bcbb380d1
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-09-01 16:42:12 +0900

    CB-358 [refactor]: add filtering allergy ingredient condition
    알러지 유발 재료인 경우 필터링 조건 추가

--- src/main/java/com/pinkdumbell/cocobob/domain/product/ProductBooleanBuilder.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/product/ProductBooleanBuilder.java
@@ -19,6 +19,11 @@ public class ProductBooleanBuilder {
             builder.and(makeIngredientBooleanBuilder(requestParameter.getIngredient()));
         }
 
+        if (requestParameter.getAllergyIngredient() != null) {
+            builder.and(
+                makeAllergyIngredientBooleanBuilder(requestParameter.getAllergyIngredient()));
+        }
+
         return builder;
     }
 
@@ -37,8 +42,68 @@ public class ProductBooleanBuilder {
                 builder.or(qproduct.duck.eq(true));
             } else if ("turkey".equals(ingredient)) {
                 builder.or(qproduct.turkey.eq(true));
+            } else if ("pork".equals(ingredient)) {
+                builder.or(qproduct.meat.eq(true));
+                //meat - > pork 데이터 베이스 스키마에는 meat로 되어 그대로 사용
+            } else if ("salmon".equals(ingredient)) {
+                builder.or(qproduct.salmon.eq(true));
+            } else if ("hydrolyticBeef".equals(ingredient)) {
+                builder.or(qproduct.hydrolyticBeef.eq(true));
+            } else if ("hydrolyticMutton".equals(ingredient)) {
+                builder.or(qproduct.hydrolyticMutton.eq(true));
+            } else if ("hydrolyticChicken".equals(ingredient)) {
+                builder.or(qproduct.hydrolyticChicken.eq(true));
+            } else if ("hydrolyticDuck".equals(ingredient)) {
+                builder.or(qproduct.hydrolyticDuck.eq(true));
+            } else if ("hydrolyticTurkey".equals(ingredient)) {
+                builder.or(qproduct.hydrolyticTurkey.eq(true));
+            } else if ("hydrolyticPork".equals(ingredient)) {
+                builder.or(qproduct.hydrolyticMeat.eq(true));
+                //meat - > pork 데이터 베이스 스키마에는 meat로 되어 그대로 사용
+            } else if ("hydrolyticSalmon".equals(ingredient)) {
+                builder.or(qproduct.hydrolyticSalmon.eq(true));
             }
+        }
 
+        return builder;
+    }
+
+    public static BooleanBuilder makeAllergyIngredientBooleanBuilder(List<String> ingredients) {
+        BooleanBuilder builder = new BooleanBuilder();
+        QProduct qproduct = QProduct.product;
+        for (String ingredient : ingredients) {
+
+            if ("beef".equals(ingredient)) {
+                builder.and(qproduct.beef.eq(false));
+            } else if ("mutton".equals(ingredient)) {
+                builder.and(qproduct.mutton.eq(false));
+            } else if ("chicken".equals(ingredient)) {
+                builder.and(qproduct.chicken.eq(false));
+            } else if ("duck".equals(ingredient)) {
+                builder.and(qproduct.duck.eq(false));
+            } else if ("turkey".equals(ingredient)) {
+                builder.and(qproduct.turkey.eq(false));
+            } else if ("pork".equals(ingredient)) {
+                builder.and(qproduct.meat.eq(false));
+                //meat - > pork 데이터 베이스 스키마에는 meat로 되어 그대로 사용
+            } else if ("salmon".equals(ingredient)) {
+                builder.and(qproduct.salmon.eq(false));
+            } else if ("hydrolyticBeef".equals(ingredient)) {
+                builder.and(qproduct.hydrolyticBeef.eq(false));
+            } else if ("hydrolyticMutton".equals(ingredient)) {
+                builder.and(qproduct.hydrolyticMutton.eq(false));
+            } else if ("hydrolyticChicken".equals(ingredient)) {
+                builder.and(qproduct.hydrolyticChicken.eq(false));
+            } else if ("hydrolyticDuck".equals(ingredient)) {
+                builder.and(qproduct.hydrolyticDuck.eq(false));
+            } else if ("hydrolyticTurkey".equals(ingredient)) {
+                builder.and(qproduct.hydrolyticTurkey.eq(false));
+            } else if ("hydrolyticPork".equals(ingredient)) {
+                builder.and(qproduct.hydrolyticMeat.eq(false));
+                //meat - > pork 데이터 베이스 스키마에는 meat로 되어 그대로 사용
+            } else if ("hydrolyticSalmon".equals(ingredient)) {
+                builder.and(qproduct.hydrolyticSalmon.eq(false));
+            }
         }
 
         return builder;
