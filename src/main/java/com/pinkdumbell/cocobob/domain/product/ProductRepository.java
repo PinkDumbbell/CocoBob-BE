3caa4c60787cd1fe04f87b1a1d54b17f522dc247
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-09-14 18:06:38 +0900

    CB-392 [feat]: able to keyword, product detail caching
    상품 검색 키워드와 상품 상세 정보를 캐싱함으로써, SQL문이 발생하지 않도록 하였습니다.

--- src/main/java/com/pinkdumbell/cocobob/domain/product/ProductRepository.java
+++ src/main/java/com/pinkdumbell/cocobob/domain/product/ProductRepository.java
@@ -1,14 +1,14 @@
 package com.pinkdumbell.cocobob.domain.product;
 
 import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface ProductRepository extends JpaRepository<Product, Long>,
     JpaSpecificationExecutor<Product>,ProductSearchQueryDsl {
 
+    @Cacheable(cacheNames = "productDetails", key = "#productId")
     @Override
     Optional<Product> findById(Long productId);
 
