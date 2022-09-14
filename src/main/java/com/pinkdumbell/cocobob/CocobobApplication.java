0c9ae0184e1ac11397dbc890e8add7e4bca2e0b5
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-09-14 17:34:53 +0900

    CB-392 [chore]: cache Logger & available cache application
    스프링에서 캐시 사용이 가능하도록 하였습니다.

--- src/main/java/com/pinkdumbell/cocobob/CocobobApplication.java
+++ src/main/java/com/pinkdumbell/cocobob/CocobobApplication.java
@@ -2,13 +2,16 @@ package com.pinkdumbell.cocobob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class CocobobApplication {
 
     static {
         System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
     }
+
     public static void main(String[] args) {
         SpringApplication.run(CocobobApplication.class, args);
     }
