c9cd5ee5a2be2fe4e48d543aee7170af1d18bd76
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-10-07 15:45:47 +0900

    CB-439,CB-444: add brand_image column to product
    상품 상세페이지 조회시 브랜드 이름과 함께 사용할 수 있는 브랜드 이미지 제공을 위해 테이블 컬럼 추가

---
+++ src/main/resources/db/migration/V14.9__add_brand_image_to_product.sql
@@ -0,0 +1,89 @@
+alter table product add column brand_image varchar(1000) null ;
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/10%EC%96%B5%EC%82%AC%EB%A3%8C.jpg" where brand = "10억사료";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/ANF.jpg" where brand = "ANF";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/HALO.jpg" where brand = "HALO";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/SHAKEAT.jpg" where brand = "SHAKEAT";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EA%B0%95%EC%95%84%EC%A7%80%EB%8C%80%ED%86%B5%EB%A0%B9.jpg" where brand = "강아지대통령";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EA%B2%8C%EB%8D%94.jpg" where brand = "게더";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EA%B5%BF%ED%94%84%EB%9E%9C%EB%93%9C.jpg" where brand = "굿프랜드";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EA%BC%AC%EC%8B%9C%EB%8B%A4.jpg" where brand = "꼬시다";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%82%98%EC%9D%B8%EC%BC%80%EC%96%B4.jpg" where brand = "나인케어";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%82%B4%EC%B6%94%EB%9F%B4%EB%B0%9C%EB%9E%80%EC%8A%A4.jpg" where brand = "내추럴발란스";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%82%B4%EC%B6%94%EB%9F%B4%ED%8C%A9%ED%86%A0%EB%A6%AC.jpg" where brand = "내추럴팩토리";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%82%B4%EC%B6%94%EB%A1%9C.jpg" where brand = "내추로";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%84%A4%EC%9D%B4%EC%B2%98%EC%8A%A4%EB%B2%84%EB%9D%BC%EC%9D%B4%EC%96%B4%ED%8B%B0.jpg" where brand = "네이처스버라이어티";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%84%A4%EC%B8%84%EB%9F%B4%EC%BD%94%EC%96%B4.jpg" where brand = "네츄럴코어";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%89%B4%EC%9B%A8%EC%9D%B4%EB%B8%8C.jpg" where brand = "뉴웨이브";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%89%B4%ED%8A%B8%EB%A1%9C.jpg" where brand = "뉴트로";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%89%B4%ED%8A%B8%EB%A6%AC%EB%82%98.jpg" where brand = "뉴트리나";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%89%B4%ED%8A%B8%EB%A6%AC%EC%86%8C%EC%8A%A4.jpg" where brand = "뉴트리소스";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%89%B4%ED%8A%B8%EB%A6%AC%ED%94%8C%EB%9E%9C.jpg" where brand = "뉴트리플랜";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%8D%94%EB%8F%85.jpg" where brand = "더독";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%8D%B0%EC%9D%B4%EC%8A%A4%ED%8F%AC.jpg" where brand = "데이스포";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%8F%84%EA%B8%B0%EB%A7%A8.jpg" where brand = "도기맨";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%8F%84%EB%B9%84.jpg" where brand = "도비";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%93%80%EB%A8%BC.jpg" where brand = "듀먼";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%94%A9%EA%B3%A0.jpg" where brand = "딩고";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%9D%BC%ED%8F%AC%EA%B7%B8.jpg" where brand = "라포그";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%9F%AD%EC%85%94%EB%A6%AC%EB%B0%9C%EB%9E%80%EC%8A%A4.jpg" where brand = "럭셔리발란스";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%A1%9C%EC%96%84%EC%BA%90%EB%8B%8C.jpg" where brand = "로얄캐닌";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%A1%9C%EC%9A%B0%EC%A6%88.jpg" where brand = "로우즈";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%A7%88%EC%9D%B4%ED%8E%AB%EB%8B%A5%ED%84%B0.jpg" where brand = "마이펫닥터";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%A7%A5%EC%95%84%EB%8B%B4%EC%8A%A4.jpg" where brand = "맥아담스";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%AA%AC%EC%A7%80.jpg" where brand = "몬지";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%B0%94%EC%9D%B4%ED%83%88%EC%97%90%EC%84%BC%EC%85%9C.jpg" where brand = "바이탈에센셜";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%B2%85%EC%8A%A4%ED%8E%AB.jpg" where brand = "벅스펫";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%B2%A0%EA%B8%B0%EB%8F%84%EA%B7%B8.jpg" where brand = "베기도그";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%B3%B4%EC%8A%A4%EB%8F%85.jpg" where brand = "보스독";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EB%B8%8C%EC%9D%B4%ED%94%8C%EB%9E%98%EB%8B%9B.jpg" where brand = "브이플래닛";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EC%8B%9C%EC%A0%80.jpg" where brand = "시저";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EC%95%84%EC%B9%B4%EB%82%98.jpg" where brand = "아카나";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EC%9B%B0%EC%B8%A0.jpg" where brand = "웰츠";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%EC%A7%80%EA%B7%B8%EB%8B%88%EC%B3%90.jpg" where brand = "지그니쳐";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%ED%95%98%EB%A6%BC%ED%8E%AB%ED%91%B8%EB%93%9C.jpg" where brand = "하림펫푸드";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%ED%9B%84%EC%83%88%EB%8F%85.jpg" where brand = "후새독";
+
+UPDATE product set brand_image = "https://cocobob-storage.s3.ap-northeast-2.amazonaws.com/brand_image/%ED%9E%90%EC%8A%A4.jpg" where brand = "힐스";
