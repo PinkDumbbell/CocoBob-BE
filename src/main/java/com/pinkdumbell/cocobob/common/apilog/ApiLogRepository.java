976499f21f96bd35294e3d30f17f33e88a0bd947
ErranderLee <dre12am14@gmail.com>
2022-10-30 18:56:11 +0900

    [CB-127] feat : 로그 히스토리 테이블 생성 ################ - 로그 레포지토리 생성

---
+++ src/main/java/com/pinkdumbell/cocobob/common/apilog/ApiLogRepository.java
@@ -0,0 +1,6 @@
+package com.pinkdumbell.cocobob.common.apilog;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApiLogRepository extends JpaRepository<ApiLog, Long> {
+}
