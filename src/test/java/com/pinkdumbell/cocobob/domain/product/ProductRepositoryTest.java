4508e4ee83e20a52b37dd034b5f661270ab7ba4e
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-11-03 02:17:33 +0900

    CB-430 [test]: test QueryDSL product test 모든 조건과 모든 정렬 방식을 사용한 QueryDSL 테스트

--- src/test/java/com/pinkdumbell/cocobob/domain/product/ProductRepositoryTest.java
+++ src/test/java/com/pinkdumbell/cocobob/domain/product/ProductRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.pinkdumbell.cocobob.domain.product;
 
 
+
 import com.pinkdumbell.cocobob.config.TestConfig;
 import com.pinkdumbell.cocobob.domain.product.dto.ProductSimpleResponseDto;
+import com.pinkdumbell.cocobob.domain.product.dto.ProductSpecificSearchWithLikeDto;
 import com.pinkdumbell.cocobob.domain.product.like.QLike;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
@@ -12,15 +14,20 @@ import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Arrays;
 import java.util.List;
+
+import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @DataJpaTest
 @Import(TestConfig.class)
@@ -29,7 +36,7 @@ class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
-    @MockBean
+    @Autowired
     ProductSearchQueryDslImpl productSearchQueryDsl;
 
     @Autowired
@@ -48,8 +55,10 @@ class ProductRepositoryTest {
     void findProductDetailById() {
 
         //ProductId 10을 가지고 있는 상품 조회
-        Product result = productRepository.findById(10L).get();
-        Assertions.assertThat(result.getId()).isEqualTo(10L);
+        Optional<Product> result = productRepository.findById(10L);
+
+        Assertions.assertThat(result.isPresent()).isEqualTo(true);
+
     }
 
     @Test
@@ -92,7 +101,47 @@ class ProductRepositoryTest {
             .leftJoin(qLike).on(qProduct.id.eq(qLike.product.id))
             .where(new BooleanBuilder())
             .fetch();
+
         Assertions.assertThat(result).isNotNull();
     }
+
+    @Test
+    @DisplayName("모든 검색 조건과 모든 정렬을 통해서 동적 쿼리를 실행할 수 있다.")
+    void search_condition_by_id_asc(){
+
+        //when
+        int page = 1;
+        int size = 10;
+        String[] sort_base = {"ID,ASC","ID,DESC","PRICE,ASC","PRICE,DESC","LIKE,ASC","LIKE,DESC"};
+        for(int i = 0 ; i < 6 ; i++){
+
+            ProductSpecificSearchWithLikeDto productSpecificSearchWithLikeDto = ProductSpecificSearchWithLikeDto.builder()
+                .ingredient(Arrays.asList("beef","mutton","chicken","duck","turkey","pork","salmon","hydrolyticBeef","hydrolyticMutton","hydrolyticChicken","hydrolyticDuck","hydrolyticTurkey","hydrolyticPork","hydrolyticSalmon"))
+                .allergyIngredient(Arrays.asList("beef","mutton","chicken","duck","turkey","pork","salmon","hydrolyticBeef","hydrolyticMutton","hydrolyticChicken","hydrolyticDuck","hydrolyticTurkey","hydrolyticPork","hydrolyticSalmon"))
+                .brands(Arrays.asList("로얄캐닌"))
+                .types(Arrays.asList("aged","growing","obesity","pregnant"))
+                .codes(Arrays.asList("101202"))
+                .keyword("로얄캐닌")
+                .aafco(true)
+                .sort(sort_base[i])
+                .page(page)
+                .size(size)
+                .build();
+
+            //given
+            Pageable pageable = PageRequest.of(productSpecificSearchWithLikeDto.getPage(), productSpecificSearchWithLikeDto.getSize());
+
+
+            //EXECUTE
+            PageImpl<ProductSimpleResponseDto> result = productRepository.findAllWithLikes(
+                productSpecificSearchWithLikeDto, 1L);
+
+            //EXPECTED
+            Assertions.assertThat(result.getPageable()).isEqualTo(pageable);
+
+        }
+    }
+
+
 
 }
