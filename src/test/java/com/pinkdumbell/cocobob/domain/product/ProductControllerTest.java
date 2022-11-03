c928da2ffbed82560a2c3193b6dbb8b3808522fb
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-11-03 16:00:35 +0900

    CB-430 [test]: refactoring test code ProductControllerTest
    스프링 부트 테스트로 실행하던 테스트를 리팩토링하여 재작성하였습니다.

--- src/test/java/com/pinkdumbell/cocobob/domain/product/ProductControllerTest.java
+++ src/test/java/com/pinkdumbell/cocobob/domain/product/ProductControllerTest.java
@@ -1,68 +1,44 @@
 package com.pinkdumbell.cocobob.domain.product;
 
-
-import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.pinkdumbell.cocobob.common.EmailUtil;
-import com.pinkdumbell.cocobob.config.MailConfig;
-import com.pinkdumbell.cocobob.domain.auth.AppleOauthInfo;
-import com.pinkdumbell.cocobob.domain.auth.GoogleOauthInfo;
-import com.pinkdumbell.cocobob.domain.auth.KakaoOauthInfo;
-import javax.transaction.Transactional;
+import com.pinkdumbell.cocobob.common.apilog.ApiLogInterceptor;
+import com.pinkdumbell.cocobob.domain.auth.JwtTokenProvider;
+import com.pinkdumbell.cocobob.domain.pet.PetService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-@Transactional
-@SpringBootTest
-@RunWith(SpringRunner.class)
-@AutoConfigureMockMvc
+@WebMvcTest(ProductController.class)
 class ProductControllerTest {
 
-    @Autowired
-    ProductRepository productRepository;
+    MockMvc mvc;
 
-    @Autowired
+    @MockBean
+    JwtTokenProvider jwtTokenProvider;
+    @MockBean
     ProductService productService;
-
     @MockBean
-    EmailUtil emailUtil;
-
-    @MockBean
-    MailConfig mailConfig;
-
+    PetService petService;
     @Autowired
     private WebApplicationContext context;
-
     @MockBean
-    GoogleOauthInfo googleOauthInfo;
-
-    @MockBean
-    KakaoOauthInfo kakaoOauthInfo;
-
-    @MockBean
-    AppleOauthInfo appleOauthInfo;
-
-    @Value("${apple.key.path}")
-    private String privateKey;
-
-    MockMvc mvc;
+    ApiLogInterceptor apiLogInterceptor;
 
     @BeforeEach
     void setUp() {
@@ -70,34 +46,58 @@ class ProductControllerTest {
             .addFilter(new CharacterEncodingFilter("UTF-8", true))
             .apply(springSecurity())
             .build();
+        given(apiLogInterceptor.preHandle(
+            any(HttpServletRequest.class),
+            any(HttpServletResponse.class),
+            any(Object.class)
+        )).willReturn(true);
     }
 
     @Test
-    @DisplayName("사용자가 검색한 조건에 맞춰 올바른 결과들을 가져올 수 있다(size:10, page:1)")
     @WithMockUser("USER")
-    void AAFCO_와_PAGE_SIZE_10_PAGE_NUMBER_1() throws Exception {
-        //EXECUTE & EXPECT
-        mvc.perform(get("/v1/products/search")
+    @DisplayName("사용자가 검색한 조건에 맞춰 올바른 결과들을 가져올 수 있다(size:10, page:1)")
+    void AAFCO_PAGE_SIZE_10_PAGE_NUMBER_1() throws Exception {
+        //EXECUTE
+        MvcResult result = mvc.perform(get("/v1/products/search")
                 .param("size", "10")
                 .param("page", "1")
                 .param("aaffco", "false"))
-            .andExpect(status().is2xxSuccessful())
-            .andExpect(jsonPath("$.data.pageSize").value(10))
-            .andExpect(jsonPath("$.data.pageNumber").value(1));
+            .andReturn();
+
+        //EXPECT
+        Assertions.assertThat(result.getResponse().getStatus()).isEqualTo(200);
+        Assertions.assertThat(result.getResponse().getContentAsString()).contains("상품 검색 성공");
     }
 
     @Test
-    @DisplayName("사용자가 검색한 조건에 맞춰 올바른 결과들을 가져올 수 있다(size:3, page:2)")
     @WithMockUser("USER")
-    void AAFCO_와_PAGE_SIZE_3_PAGE_NUMBER_2() throws Exception {
-        //EXECUTE & EXPECT
-        mvc.perform(get("/v1/products/search")
+    @DisplayName("사용자가 검색한 조건에 맞춰 올바른 결과들을 가져올 수 있다(size:3, page:2)")
+    void AAFCO_PAGE_SIZE_3_PAGE_NUMBER_2() throws Exception {
+
+        //EXECUTE
+        MvcResult result = mvc.perform(get("/v1/products/search")
                 .param("size", "3")
                 .param("page", "2")
                 .param("aaffco", "true"))
-            .andExpect(status().is2xxSuccessful())
-            .andExpect(jsonPath("$.data.pageSize").value(3))
-            .andExpect(jsonPath("$.data.pageNumber").value(2));
+            .andReturn();
+
+        //EXPECT
+        Assertions.assertThat(result.getResponse().getStatus()).isEqualTo(200);
+        Assertions.assertThat(result.getResponse().getContentAsString()).contains("상품 검색 성공");
     }
 
+    @Test
+    @WithMockUser("USER")
+    @DisplayName("사용자가 좋아요한 상품들을 가져왔습니다.")
+    void get_user_liked_product_information() throws Exception {
+
+        //EXECUTE
+        MvcResult result = mvc.perform(get("/v1/products/wishlist"))
+            .andReturn();
+
+        //EXPECT
+        Assertions.assertThat(result.getResponse().getStatus()).isEqualTo(200);
+        Assertions.assertThat(result.getResponse().getContentAsString()).contains("찜한 상품 불러오기 성공");
+
+    }
 }
