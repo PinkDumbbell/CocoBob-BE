30943e37c716f4b15487a173f1933965d18d6a01
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-11-04 14:09:41 +0900

    CB-430 [test]: Product Service test elasticSearchProducts
    elasticSearchProducts에 대한 테스트 코드 작성

--- src/test/java/com/pinkdumbell/cocobob/domain/product/ProductServiceTest.java
+++ src/test/java/com/pinkdumbell/cocobob/domain/product/ProductServiceTest.java
@@ -6,6 +6,7 @@ import com.pinkdumbell.cocobob.domain.product.dto.FindAllResponseDto;
 import com.pinkdumbell.cocobob.domain.product.dto.ProductDetailResponseDto;
 import com.pinkdumbell.cocobob.domain.product.dto.ProductKeywordDto;
 import com.pinkdumbell.cocobob.domain.product.dto.ProductSimpleResponseDto;
+import com.pinkdumbell.cocobob.domain.product.dto.ProductSpecificSearchDto;
 import com.pinkdumbell.cocobob.domain.product.dto.ProductSpecificSearchWithLikeDto;
 import com.pinkdumbell.cocobob.domain.product.like.Like;
 import com.pinkdumbell.cocobob.domain.product.like.LikeRepository;
@@ -28,6 +29,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -200,17 +202,131 @@ class ProductServiceTest {
                     .build()
             );
         }
-        Pageable pageable = PageRequest.of(page, 10);
-        Page<ProductSimpleResponseDto> dummyData = new PageImpl<>(dummySimpleResponse, pageable,
+        Pageable pageable = PageRequest.of(page, size);
+        PageImpl<ProductSimpleResponseDto> dummyData = new PageImpl<>(dummySimpleResponse, pageable,
             size);
 
         given(userRepository.findByEmail(anyString())).willReturn(Optional.of(testUser));
 
-        given(productRepository.findAllRelatedProductsById(any(), any())).willReturn(
-            (PageImpl<ProductSimpleResponseDto>) dummyData);
+        given(productRepository.findAllRelatedProductsById(any(), any())).willReturn(dummyData);
 
         //EXECUTE
         FindAllResponseDto result = productService.getRelationProduct(1L, "test@test.com");
+
+        //EXPECT
+        Assertions.assertThat(result).isNotNull();
+    }
+
+    @Test
+    @DisplayName("사용자가 좋아한 상품 정보들 가져올 수 있다.")
+    void get_info_user_wish_list() {
+
+        //given
+        int page = 1;
+        int size = 10;
+        User testUser = User.builder()
+            .username("tester")
+            .email("test@test.com")
+            .accountType(AccountType.OWN)
+            .password("password")
+            .build();
+        List<Product> dummySimpleResponse = new ArrayList<>();
+        for (Long i = 0L; i < 10L; i++) {
+
+            dummySimpleResponse.add(
+                Product
+                    .builder()
+                    .id(i)
+                    .code("101201")
+                    .name("하림")
+                    .brand("로얄캐닌")
+                    .category("건식")
+                    .description("맛있어요")
+                    .isAAFCOSatisfied(true)
+                    .aged(true)
+                    .growing(false)
+                    .pregnant(false)
+                    .obesity(true)
+                    .thumbnail("URL:PATH")
+                    .build()
+            );
+        }
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Product> dummyData = new PageImpl<>(dummySimpleResponse, pageable,
+            size);
+
+        given(userRepository.findByEmail(anyString())).willReturn(Optional.of(testUser));
+        given(likeRepository.findAllByUserLike(any(User.class), any(Pageable.class))).willReturn(
+            dummyData);
+
+        //EXECUTE
+        FindAllResponseDto result = productService.findAllWishList("test@test.com", pageable);
+
+        //EXPECT
+        Assertions.assertThat(result).isNotNull();
+
+    }
+
+    @Test
+    @DisplayName("조건에 맞추어 상품을 가져올 수 있다.")
+    void correct_working_elastic_search_products() {
+        //given
+        int page = 1;
+        int size = 10;
+        ProductSpecificSearchDto request = ProductSpecificSearchDto.builder()
+            .code("101101")
+            .name("더리얼밀그레인프리닭고기60g6개")
+            .brand("하림펫푸드")
+            .description("소고기 함유")
+            .aafco(true)
+            .beef(true)
+            .mutton(true)
+            .chicken(true)
+            .duck(true)
+            .turkey(true)
+            .pork(true)
+            .salmon(true)
+            .hydrolyticBeef(true)
+            .hydrolyticMutton(true)
+            .hydrolyticChicken(true)
+            .hydrolyticDuck(true)
+            .hydrolyticTurkey(true)
+            .hydrolyticPork(true)
+            .hydrolyticSalmon(true)
+            .aged(true)
+            .growing(true)
+            .pregnant(true)
+            .obesity(true)
+            .build();
+
+        List<Product> dummyResponse = new ArrayList<>();
+        for (Long i = 0L; i < 10L; i++) {
+
+            dummyResponse.add(
+                Product
+                    .builder()
+                    .code("101201")
+                    .name("하림")
+                    .brand("로얄캐닌")
+                    .category("건식")
+                    .description("맛있어요")
+                    .isAAFCOSatisfied(true)
+                    .aged(true)
+                    .growing(false)
+                    .pregnant(false)
+                    .obesity(true)
+                    .thumbnail("URL:PATH")
+                    .build()
+            );
+        }
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Product> dummyData = new PageImpl<>(dummyResponse, pageable, size);
+
+        given(productRepository.findAll(any(Specification.class), any(Pageable.class))).willReturn(
+            dummyData);
+
+        //EXECUTE
+        FindAllResponseDto result = productService.elasticSearchProducts(request, pageable);
 
         //EXPECT
         Assertions.assertThat(result).isNotNull();
